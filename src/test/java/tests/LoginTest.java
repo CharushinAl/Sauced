dac329e6244d2e943d410b07e7bb10509937b2bc
sasha <charushinas@gmail.com>
2025-11-26 22:33:39 +0300

    changed LoginTest, added LoginPage, ProductPage, BaseTest

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -1,24 +1,27 @@
 package tests;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
 
-public class LoginTest {
-    // open browser
-    // input web site//
+public class LoginTest extends BaseTest {
+
+
+
     @Test
-    public void incorrectLogin() {
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://www.saucedemo.com/");
-        driver.findElement(By.xpath("//input[@name='user-name']")).sendKeys("locked_out_user");
-        driver.findElement(By.id("password")).sendKeys("secret_sauce");
-        driver.findElement(By.xpath("//input[@type='submit']")).click();
-        String errorText = driver.findElement(By.cssSelector("*[data-test='error']")).getText();
-        assertEquals(errorText, "Epic sadface: Sorry, this user has been locked out.", "Неверное");
-        driver.quit();
+    public void checkCorrectLogin() throws InterruptedException {
+        loginPage.open();
+        loginPage.login("standard_user", "secret_sauce");
+
+        assertTrue(productPage.isPageLoaded(), "Error message does not appear");
+    }
+
+    @Test
+    public void checkIncorrectLogin() {
+        loginPage.open();
+        loginPage.login("locked_out_user", "secret_sauce");
+
+        assertEquals(loginPage.errorMessageText(), "Epic sadface: Sorry, this user has been locked out.");
     }
 }
